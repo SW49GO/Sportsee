957884dd4050ef1223da93e2033334f6f31e0eeb
SW49GO <gohard.so49@gmail.com>
2023-10-21 12:55:14 +0200

    RadarChart adjust outerRadius, RadialBarChart circle

--- src/components/RadarChart/RadarChart.jsx
+++ src/components/RadarChart/RadarChart.jsx
@@ -1,28 +1,27 @@
-import { RadarChart,Radar, ResponsiveContainer,PolarAngleAxis,PolarGrid} from "recharts";
+import { RadarChart,Radar, ResponsiveContainer,PolarAngleAxis,PolarGrid, PolarRadiusAxis} from "recharts";
 import { useState, useContext,useEffect } from "react";
 import { Context } from '../../context/Context';
 import { fetchData } from "../../services/api";
 import Styles from '../../styles/RadarChart.module.css'
 import Error from "../Error";
 
+/**
+ * Function to build component
+ * @returns Component RadarChart
+ */
 function RadarCharts(){
-     // récupération de l'id du context
+// Retrieving the Context id
      const {selectedUserId } = useContext(Context);
      const [datas, setDatas] = useState(null)
    
-
-   
-     // récupération des données de l'utilisteur
+// Retrieving user data
      useEffect(()=>{
        fetchData(selectedUserId, setDatas, "performance")
      },[selectedUserId])
-     // creation d'un objet pour reformater les données à partir de (kind et data)
+     // Creating an object to reformat data from (kind and data)
     let newDatas={}
-    
     if(datas!==null){
-        console.log('datasRADAR:', datas)
         const kindDatas = datas.kind
-
         newDatas = {
             data: datas.data.map((dataItem) => {
                 return {
@@ -31,22 +30,48 @@ function RadarCharts(){
                 };
             })
         };
-        //inverse l'ordre des objets de kind dans data
+        // Reverse the order of kind objects in data
         newDatas.data.reverse();
     }
+
+    // Adjust the outerRadius of the RadarChart according to the screen size
+     const [outerRadius, setOuterRadius] = useState(75);
+
+     useEffect(() => {
+       const handleResize = () => {
+         const screenWidth = window.innerWidth;
+         const screenHeight = window.innerHeight;
+         if (screenWidth <= 1024) {
+           setOuterRadius(40);
+         } else {
+            if(screenHeight===780){
+                setOuterRadius(55)
+            }else{
+                setOuterRadius(65);
+            }
+         }
+       };
+       // Event to detect screen size change
+       window.addEventListener('resize', handleResize);
+          handleResize();
+       // Removed events when the component is dismount
+       return () => {
+         window.removeEventListener('resize', handleResize);
+       };
+     }, []);
 
      if(datas){
         return(
             <div className={Styles.radarChart}>
                 <ResponsiveContainer  width="100%" height="100%" className={Styles.container}>
-                    <RadarChart data={newDatas.data} outerRadius={80}>
-                        {/* sans ligne vertical vers le centre */}
+                    <RadarChart data={newDatas.data} outerRadius={outerRadius}>
+                        {/* sans ligne vertical du centre vers l'extérieur*/}
                         <PolarGrid radialLines={false}/>
-                        {/*  tickFormatter={(value) => value.charAt(0).toUpperCase() + value.slice(1)} mettre la 1ere lettre en majuscule de value // dy-> recentrage des datas*/}
-                        <PolarAngleAxis dataKey="kind" axisLine={false} tickLine={false} dy={2} tick = {{fill:'#FFF', fontSize: 12, fontWeight: 500}} tickFormatter={(value) => value.charAt(0).toUpperCase() + value.slice(1)}/>
-                        {/* Affichage des graduations -><PolarRadiusAxis angle={30} domain={[0, 150]} /> */}
-                        <Radar dataKey="value" stroke="#FF0101" fill="#FF0101" fillOpacity={0.7}d/>
-                        {/* <Legend /> */}
+                        {/* 5 toiles sans gratuations ni traits */}
+                        <PolarRadiusAxis tickCount={6} tick={false} axisLine={false}/>
+                        {/*  tickFormatter-> fonction pour mettre la 1ere lettre en majuscule de value // dy-> recentrage des datas// tick->chaque catégorie des datas*/}
+                        <PolarAngleAxis dataKey="kind" dy={2} tick = {{fill:'#FFF', fontSize: 12, fontWeight: 500}} tickFormatter={(value) => value.charAt(0).toUpperCase() + value.slice(1)}/>
+                        <Radar dataKey="value" stroke="#FF0101" fill="#FF0101" fillOpacity={0.7}/>
                     </RadarChart>
                 </ResponsiveContainer>
             </div>
--- src/components/RadialBarChart/RadialBarChart.jsx
+++ src/components/RadialBarChart/RadialBarChart.jsx
@@ -5,36 +5,39 @@ import { fetchMainData } from '../../services/api'
 import Styles from '../../styles/RadialBarChart.module.css'
 import Error from '../Error'
 
+/**
+ * Function to build component
+ * @returns components RadialBarChart
+ */
 function RadialBarCharts(){
      const {selectedUserId } = useContext(Context);
-    //  console.log('selectedUserIdRADIAL:', selectedUserId)
      const [datas, setDatas] = useState(null)
 
-     // récupération des données de l'utilisteur
+// Retrieving user data
      useEffect(()=>{
        fetchMainData(selectedUserId, setDatas)
      },[selectedUserId])
 
 if (datas){
- const dataArray = [{ name: datas.score * 100 + '%', value: datas.score * 100, fill: "#ff0000"}]
+ const dataArray = [{ name: datas.score * 100 + '%', value: datas.score * 100, fill:'#ff0000'}]
 
     return(
       <div className={Styles.radialBarChart}>
         <h3 className={Styles.title}>Score</h3>
-        {/* <div className={Styles.circle}></div> */}
+        <svg width="60%" height="60%" className={Styles.svg}>
+            <circle cx="50%" cy="50%" r="40%" fill="#fff"/>
+        </svg>
         <div className={Styles.legend}><p>{dataArray[0].name}</p><p>de votre objectif</p></div>
             <ResponsiveContainer width="100%" height="100%" className={Styles.container}>
                 <RadialBarChart 
-                    width={258} 
-                    height={263} 
                     data={dataArray} 
-                    innerRadius="70%" 
-                    outerRadius="70%" 
+                    innerRadius="60%" 
+                    outerRadius="60%" 
                     barSize={10}
                     startAngle={210}
                     endAngle={210 - (datas.score * 360)}
                     >
-                    <RadialBar minAngle={0} cornerRadius={10} background clockWise={true} dataKey='value'/>
+                    <RadialBar minAngle={0} cornerRadius={10} background clockWise={true} dataKey='value' />
                   </RadialBarChart>
             </ResponsiveContainer>
       </div>
